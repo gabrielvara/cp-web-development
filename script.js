2c56ad0414dd4e38a15f51a854b68674cbd20f24
Gabriel Vara <gabriel.alvesvara@outlook.com>
2024-04-23 19:28:56 -0300

    Implementaçao da página de login

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function clickSim() {
-    window.location.href = "home.html";
+    window.location.href = "página-login.html";
 }
 
 function clickNao() {
     window.location.href = "https://www.youtube.com/@luccastoon";
+}
+
+function clickLogin() {
+    var erro = document.getElementById("erro");
+    var usuario = document.getElementById("usuario").value 
+    var senha = document.getElementById("senha").value 
+    if (usuario == "1234" && senha == "1234" ){
+        window.location.href = "home.html";
+    }
+    else {
+        erro.innerText = "Usuário ou Senha incorretos, Tente novamente"
+    }
 }
